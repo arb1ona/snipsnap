8a9c5a6457a57b4807c984d362b35f468ec5df5d
Arbiona <arbiona.shahu@gmail.com>
2024-08-06 16:30:02 +0200

    Add responsive `Navbar` & `CallToAction`

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,73 @@
-import Image from "next/image";
+import { mainColor } from "@/Colors";
+import DataObjectIcon from "@mui/icons-material/DataObject";
 
 export default function Home() {
-  return <div className="poppins"></div>;
+  return (
+    <div>
+      <Navbar />
+      <CTASection />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="flex m-5 max-sm:mt-9 mx-8 items-center justify-between max-sm:flex-col">
+      <Logo />
+      <Buttons />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex gap-2 items-center">
+      <div className={`bg-mainColor p-[6px] rounded-md`}>
+        <DataObjectIcon sx={{ fontSize: 30, color: "white" }} />
+      </div>
+      <div className="flex gap-1 text-[19px]">
+        <span className={`font-bold text-mainColor`}>Snip</span>
+        <span className="text-slate-600">Snap</span>
+      </div>
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className="flex gap-2 max-sm:flex-col max-sm:w-[60%] max-sm:mt-8">
+      <button
+        className={`max-sm:w-full bg-mainColor p-[8px] px-6 text-sm text-white rounded-md`}
+      >
+        Sign In
+      </button>
+      <button
+        className={`text-sm border border-mainColor text-mainColor\hover:bg-mainColor hover:text-white p-[8px] px-6 rounded-md`}
+      >
+        Sign Up
+      </button>
+    </div>
+  );
+}
+
+function CTASection() {
+  return (
+    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6">
+      <h2 className="font-bold text-2xl text-center">
+        Organize Your Code Snippets
+        <span className={`text-mainColor`}> Efficiently!</span>
+      </h2>
+      <p className="text-center text-sm w-[450px] max-sm:w-full text-slate-500">
+        With our advanced tagging and search features, you can quickly find the
+        snippet you need, right when you need it. Spend less time searching for
+        code and more time writing it.
+      </p>
+      <button
+        className={`block px-9 py-3 text-sm font-medium text-mainColor transition focus:outline-none`}
+        type="button"
+      >
+        Let's get started!
+      </button>
+    </div>
+  );
 }
