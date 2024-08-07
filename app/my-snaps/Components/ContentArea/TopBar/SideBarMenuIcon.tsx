9a2ae43384a5e2fae8e7b4313b36dfc9c6084db6
Arbiona <arbiona.shahu@gmail.com>
2024-08-07 19:31:36 +0200

    import GlobalContext

---
+++ app/my-snaps/Components/ContentArea/TopBar/SideBarMenuIcon.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { useGlobalContext } from "@/app/context/GlobalContext";
+
+function SideBarMenuIcon() {
+  const {
+    openSideBarObject: { openSideBar, setOpenSideBar },
+  } = useGlobalContext();
+  return (
+    <>
+      {!openSideBar ? (
+        <MenuOutlinedIcon
+          onClick={() => setOpenSideBar(!openSideBar)}
+          className="text-slate-500 cursor-pointer hidden max-md:block"
+        />
+      ) : (
+        <CloseOutlinedIcon
+          onClick={() => setOpenSideBar(!openSideBar)}
+          className="text-slate-500 cursor-pointer hidden max-md:block"
+        />
+      )}
+    </>
+  );
+}
+
+export default SideBarMenuIcon;
